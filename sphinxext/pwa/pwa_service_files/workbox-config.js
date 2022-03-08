a74dcb1d6bf827fc58d40ae861ee5518a59bb449
Restyled.io <commits@restyled.io>
2022-03-08 12:58:25 +0000

    Restyled by clang-format

--- sphinxext/pwa/pwa_service_files/workbox-config.js
+++ sphinxext/pwa/pwa_service_files/workbox-config.js
@@ -1,11 +1,8 @@
 module.exports = {
-	globDirectory: '.',
-	globPatterns: [
-		'**/*.{csv,vi,pdf,zip,png,jpg,svg,css,json,txt,webmanifest,eot,ttf,woff,woff2,js,ico,html,inv}'
-	],
-	swDest: 'sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory : '.',
+  globPatterns : [
+    '**/*.{csv,vi,pdf,zip,png,jpg,svg,css,json,txt,webmanifest,eot,ttf,woff,woff2,js,ico,html,inv}'
+  ],
+  swDest : 'sw.js',
+  ignoreURLParametersMatching : [ /^utm_/, /^fbclid$/]
 };
