f2b1bf1bab5729b02aa4acacff280d13145ce056
Restyled.io <commits@restyled.io>
2022-03-08 12:58:28 +0000

    Restyled by prettier

--- sphinxext/pwa/pwa_service_files/workbox-config.js
+++ sphinxext/pwa/pwa_service_files/workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  globDirectory : '.',
-  globPatterns : [
-    '**/*.{csv,vi,pdf,zip,png,jpg,svg,css,json,txt,webmanifest,eot,ttf,woff,woff2,js,ico,html,inv}'
+  globDirectory: ".",
+  globPatterns: [
+    "**/*.{csv,vi,pdf,zip,png,jpg,svg,css,json,txt,webmanifest,eot,ttf,woff,woff2,js,ico,html,inv}",
   ],
-  swDest : 'sw.js',
-  ignoreURLParametersMatching : [ /^utm_/, /^fbclid$/]
+  swDest: "sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
